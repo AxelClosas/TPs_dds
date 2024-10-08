3d169794379ded0fcad2b06e43d08b2f902fc4a5
Axel Closas <axelclosas13@gmail.com>
2024-10-07 23:28:47 -0300

    Pequeños avances en la lógica del inicio del juego

--- TP3/Ejercicio_2.c
+++ TP3/Ejercicio_2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define T 5
 #define cant_fichas 3
@@ -9,21 +10,95 @@ typedef struct tab
 {
     int jugador;
     int tablero[T][T];
+    int vidas;
 
 } TABLERO;
 
 void pintar_tablero(TABLERO *jug);
 void ubicar_fichas(TABLERO *jug);
+void iniciar_juego(TABLERO *jug1, TABLERO *jug2);
+
 
 void main()
 {
+    int jugar;
 
-    TABLERO J1 = {1, {{0}} };
-    TABLERO J2 = {2, {{0}} };
 
-    ubicar_fichas(&J1);
-    ubicar_fichas(&J2);
+    do
+    {
+        TABLERO J1 = {1, {{0}}, 3 };
+        TABLERO J2 = {2, {{0}}, 3 };
+        ubicar_fichas(&J1);
+        ubicar_fichas(&J2);
 
+        system("PAUSE");
+        system("CLS");
+
+
+        iniciar_juego(&J1, &J2);
+
+        printf("Volver a jugar?\n\n\t1. SI\n\t0. NO\n\n\tOpcion: ");
+        scanf("%i", &jugar);
+    } while (jugar == 1);
+
+}
+
+int turno_aleatorio()
+{
+    srand(time(0));
+
+    int turno = (rand() % 2) + 1;
+
+    return turno;
+}
+
+int atacar_coordenada(TABLERO *jug, int fila, int columna)
+{
+    int i, j;
+    int golpe;
+
+    for (i=0; i < T; i++)
+        for (j=0; j < T; j++)
+        {
+            if (jug->tablero[fila - 1][columna - 1] == 1)
+            {
+                jug->tablero[fila - 1][columna - 1] = 0;
+                golpe = 1;
+            } else
+            {
+                golpe = 0;
+            }
+        }
+
+    return golpe;
+}
+
+void iniciar_juego(TABLERO *jug1, TABLERO *jug2)
+{
+    int turno_inicial = turno_aleatorio();
+
+    int fila, columna, golpe;
+
+    printf("Jugador [%i]. Ingresa tus coordenadas de ataque...\n", turno_inicial);
+    printf("Fila: ");
+    scanf("%i", &fila);
+
+    printf("\n");
+
+    printf("Columna: ");
+    scanf("%i", &columna);
+
+    do
+    {
+        if (turno_inicial == 1)
+        {
+            golpe = atacar_coordenada(jug2, fila, columna);
+
+        }
+        else{
+
+        }
+    } while ( (jug1->vidas == 0) || (jug2->vidas == 0) );
 
 }
 
