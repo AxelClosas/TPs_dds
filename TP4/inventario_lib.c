a3a9e06c9395bd0873a62a5e8ea5b556349cd426
Axel Closas <axelclosas13@gmail.com>
2024-10-27 23:54:52 -0300

    El codigo tiene 2 funciones, 1 para cargar los datos en el array de estructuras y la otra para escribir en el archivo de texto.

--- TP4/inventario_lib.c
+++ TP4/inventario_lib.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 
+typedef struct {
+	char titulo[50];
+	char autor[50];
+	int anio;
+
+}libro;
+
+void agregar_libro(libro *p);
+int escribir_libros(libro p[], int cant);
 
 int main()
 {
